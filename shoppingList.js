05a6540d891b48a9a898b997e9e97338c13684c4
yovavsanders <45664116+yovavsanders@users.noreply.github.com>
2020-07-25 22:58:18 +0300

    Update shoppingList.js

--- shoppingList.js
+++ shoppingList.js
@@ -1,84 +1,109 @@
-db.collection('buylist').get().then((snapshot)=> {
-    snapshot.docs.forEach(doc => {
-        console.log(doc.data().item_name);
-        console.log(doc.data().date);
-        var item = {
-            title: doc.data().item_name,
-            date_added: doc.data().date,
-            done: false,
-            data_id: doc.id
-        }
-        app.todos.push(item);
-    });
-});
+<!DOCTYPE html>
+<html style='width:100%; height:100%;'>
+<style src="shoppingList.css"></style>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
+  <meta http-equiv="X-UA-Compatible" content="IE=7,IE=9" /> 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>רשימת קניות</title>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.16.1/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.16.1/firebase-firestore.js"></script>
 
-const app = new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data () {
-      return {
-        isDark:true,
-        show: true,
-        newTodo: '',
-        todo: [],
-        todos: [],
-        today: new Date().getDate()+"/" + new Date().getMonth()+"/"+new Date().getFullYear(),
-        day: this.todoDay(),
-        date: new Date().getDate(),
-        ord: this.nth(new Date().getDate()),
-        year: new Date().getFullYear(),
-      }
-    },
-    methods: {
-      addTodo() {
-        var value = this.newTodo && this.newTodo.trim();
-        if (!value) {
-          return;
-        }
-        this.todos.push({
-          title: this.newTodo,
-          done: false,
-          date_added: this.today
-        });
-        db.collection('buylist').add(
-            {
-                item_name: this.newTodo,
-                done: false,
-                date: this.today
-            }
-        ).then(function(docRef) {
-            if (app.todos.length > 0){
-                app.todos[app.todos.length - 1].data_id =  docRef.id;
-            }
-        });
-        this.newTodo = '';
-      },
-      removeTodo(index) {
-        db.collection('buylist').doc(this.todos[index].data_id).delete();
-        this.todos.splice(index, 1);
+<script src="https://polyfill.io/v3/polyfill.js?features=es5,es6,es7&flags=gated"></script>  
+<link rel= 'stylesheet' href= 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'> 
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/@mdi/font@3.x/css/materialdesignicons.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 
-        
-      },
-      todoDay() {
-        var d = new Date();
-        var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-        return d.getDay();
-      },
-      nth(d) {
-        if(d>3 && d<21) return 'th';
-        switch (d % 10) {
-          case 1:  return "st";
-          case 2:  return "nd";
-          case 3:  return "rd";
-          default: return "th";
-        }
-      },
-    },
-    filters: {
-    capitalize: function (value) {
-      if (!value) return ''
-      value = value.toString()
-      return value.charAt(0).toUpperCase() + value.slice(1)
-    }
-  }
-  });
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+<script src="https://polyfill.io/v3/polyfill.js?features=es5,es6,es7&flags=gated"></script> 
+
+</head>
+
+<body style='width:100%; height:95%;' scrolling='no' dir="rtl">
+<div id="app">
+    <v-app right style="
+    background-image: url('https://images.unsplash.com/photo-1515706886582-54c73c5eaf41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80');">
+      <v-main>
+
+        <v-container>
+          <v-layout row justify-center class="ma-5">
+            <v-flex xs12 sm8>
+
+              <v-card>
+                <v-toolbar color="blue darken-4" dark>
+                  <v-toolbar-title class="headline">רשימת קניות שלומית ויובב</v-toolbar-title>
+                </v-toolbar>
+                <v-list two-line subheader>
+                  <v-container>
+                    <v-subheader class="headline">{{today}}</v-subheader>
+                    <v-spacer></v-spacer>
+  
+                    <p class="text-xs-right"><b>{{todos.length}}</b> מוצרים</p>
+  
+                    <v-flex xs12>
+                      <v-combobox right clearable v-model="newTodo" :items="historylist" id="newTodo" name="newTodo" placeholder="הוספת מוצר חדש" @keyup.enter="addTodo"
+                        :search-input.sync="search"
+                        hide-selected
+                      ></v-combobox>
+                    </v-flex>
+                  </v-container>
+                  <v-subheader class="subheading" v-if="todos.length == 0">יש 0 מוצרים ברשימה. הוסף מוצר</v-subheader>
+                  <div v-for="(todo, i) in todos">
+                    <v-list-item>
+                      <v-list-item-action>
+                        <v-checkbox v-model="todo.done" @change="cbxChanged(i)"></v-checkbox>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <v-list-item-title :class="{
+                    done: todo.done
+                    }" class="title">{{todo.title | capitalize}}
+                        </v-list-item-title>
+                        <v-list-item-subtitle>נוסף ב: {{todo.date_added}}</v-list-item-subtitle>
+                      </v-list-item-content>
+                      <v-btn icon ripple color="red" model="todo.checked"  v-if="todo.done" @click="removeTodo(i)">
+                        <v-icon>mdi-close</v-icon>
+                      </v-btn>
+                    </v-list-item>
+                  </div>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container >
+      </v-main>
+    </v-app>
+  </div>
+
+  
+<script>
+
+
+
+    // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyALRhddmPlSwsLhu5AM2zfH9vBOdt2nl9c",
+        authDomain: "shoppinglistyovavshlomit.firebaseapp.com",
+        databaseURL: "https://shoppinglistyovavshlomit.firebaseio.com",
+        projectId: "shoppinglistyovavshlomit",
+        storageBucket: "shoppinglistyovavshlomit.appspot.com",
+        messagingSenderId: "853766808794",
+        appId: "1:853766808794:web:a7214f361f0924ea23f1e3",
+        measurementId: "G-C2M54TN4KG"
+          };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      const db = firebase.firestore();
+  
+  
+  
+  
+  </script>
+  <script src="shoppingList.js"></script>
+</body>
+</html>
